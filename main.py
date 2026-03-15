7f9e05d5bd5313d03e47d0f7ce859d9fac8b4879
osantonio <jlant1991@gmail.com>
2026-03-15 02:52:09 -0500

    obligaciones de dokploy

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # ./main.py
 
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    # Escuchar en 0.0.0.0 es obligatorio en Docker/Dokploy
+    # El puerto lo ideal es que sea dinámico o coincida con Dokploy
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host='0.0.0.0', port=port, debug=False)
