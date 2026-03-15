b30e3b606f75bf1f20febeadbb8f5ab44e4b60f9
osantonio <jlant1991@gmail.com>
2026-03-15 04:01:49 -0500

    config

--- app/config/config.py
+++ app/config/config.py
@@ -6,10 +6,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY")
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "")
+    if not SECRET_KEY:
+        raise ValueError("La variable de entorno SECRET_KEY debe estar configurada")
     DATABASE_URL = os.getenv("DATABASE_URL")
     DEBUG = os.getenv("DEBUG") == "True"
     JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
-    JWT_EXPIRATION_MINUTES = int(os.getenv("JWT_EXPIRATION_MINUTES"))
+    JWT_EXPIRATION_MINUTES = int(os.getenv("JWT_EXPIRATION_MINUTES", "30"))
     
 config = Config()
